7cb534c11d2c133ae36cf43e7b049cd17fa14be7
oanaciuca <oana.daniela.ciuca@gmail>
2024-09-11 17:23:28 +0200

    initilisation la-maison-jungle

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import Banner from './Banner';
+import Cart from './Cart';
+
+function App() {
+    return (
+        <>
+            <Banner />
+            <Cart />
+        </>
+    );
+}
+
+export default App;
